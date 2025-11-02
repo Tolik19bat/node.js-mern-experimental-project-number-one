6748834a8b833a74b8ad90ba6a756c6d75d25d46
Anatoly Batov <play.hoffa@gmail.com>
2025-11-02 22:13:03 +0400

    02.11.25-correct-format-file-and-code-app.js

--- app.mjs
+++ app.mjs
@@ -1,5 +1,8 @@
+import chalk from 'chalk';                                      
+
+
 if(process.argv.length < 3) {
-    console.log("Укажите величину в дюймах");
+    console.log(chalk.red("Укажите величину в дюймах"));
     process.exit(1);
 }
 const name = process.argv[2];
@@ -7,9 +10,9 @@ const inches = parseFloat(process.argv[3]);
 console.log(`Hello, name ${name}, inches ${inches}!`);
 
 if (!inches || isNaN(inches)) {
-    console.log("Укажите корректную величину в дюймах в виде числа");
+    console.error(chalk.red("Укажите корректную величину в дюймах в виде числа"));
     process.exit(1);
 }
 
 const centimeters = inches * 2.54;
-console.log(`${inches} дюймов = ${centimeters} сантиметров`);
+console.log(`${chalk.blue(inches)} дюймов = ${chalk.green(centimeters)} сантиметров`);
