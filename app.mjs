78652e5410fe067ad6dddb9ececb7a3b465b30c1
Anatoly Batov <play.hoffa@gmail.com>
2025-11-04 17:11:02 +0400

    04.11.25-dotenv-and-environment-variable-app.mjs

--- app.mjs
+++ app.mjs
@@ -1,18 +1,27 @@
-import chalk from 'chalk';                                      
+import chalk from "chalk";
+import { config } from "dotenv";
 
+config();
 
-if(process.argv.length < 3) {
-    console.log(chalk.red("Укажите величину в дюймах"));
-    process.exit(1);
+if (process.argv.length < 3) {
+  console.log(chalk.red("Укажите величину в дюймах"));
+  process.exit(1);
 }
 const name = process.argv[2];
 const inches = parseFloat(process.argv[3]);
 console.log(`Hello, name ${name}, inches ${inches}!`);
 
 if (!inches || isNaN(inches)) {
-    console.error(chalk.red("Укажите корректную величину в дюймах в виде числа"));
-    process.exit(1);
+  console.error(chalk.red("Укажите корректную величину в дюймах в виде числа"));
+  process.exit(1);
 }
 
 const centimeters = inches * 2.54;
-console.log(`${chalk.blue(inches)} дюймов = ${chalk.green(centimeters)} сантиметров`);
+if (process.env.COLORIZE && process.env.COLORIZE === "no") {
+  console.log(`${inches} дюймов = ${centimeters} сантиметров`);
+} else {
+  console.log(
+    `${chalk.blue(inches)} дюймов = ${chalk.green(centimeters)} сантиметров`
+  );
+}
+console.log("DEBUG: COLORIZE raw =", JSON.stringify(process.env.COLORIZE));
